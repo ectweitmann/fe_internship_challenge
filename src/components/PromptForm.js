9cfdcd011b3ff5acd7c197fbd15ec7e0204a5ba6
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-05-17 15:03:39 -0700

    Remove import for useEffect

--- src/components/PromptForm.js
+++ src/components/PromptForm.js
@@ -1,4 +1,4 @@
-import React,{ useState, useEffect } from 'react';
+import React,{ useState } from 'react';
 import { postPrompt } from '../apiCalls';
 import inactiveAI from '../assets/inactiveAI.png';
 import activeAI from '../assets/activeAI.png';
